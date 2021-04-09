257d6ba2218ddd82cdf97f4d67f67cd16f6322df
philux58 <philippe.wauters@opencom.eu>
2021-04-09 10:11:49 +0200

    Update test_sum.py

--- Lesson4/ActivityA/test_sum.py
+++ Lesson4/ActivityA/test_sum.py
@@ -5,7 +5,7 @@ from sum import *
 class TestSum(unittest.TestCase):
 
     def test_sum_1(self):
-        self.assertEqual(add_numbers(1,2), 3, "expected sum of 1 and 2 to be 3 but got " + str(add_numbers(1,2)))
+        self.assertEqual(add_numbers(1,2), 4, "expected sum of 1 and 2 to be 4 but got " + str(add_numbers(1,2)))
 
     def test_sum_2(self):
         self.assertEqual(add_numbers(10,20), 30, "expected sum of 10 and 20 to be 30 but got " + str(add_numbers(10,20)))
